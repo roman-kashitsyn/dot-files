1ecdf065e30da00c4c625bd46f7833b29e703c87
Roman Kashitsyn <roman.kashitsyn@gmail.com>
2013-07-20 11:53:54 +0400

    Move keybindings into a separate module, fix clang scripts lazy-load

---
+++ .emacs.d/modules/my-keybindings.el
@@ -0,0 +1,23 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Keybindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "C-x \\") 'align-regexp)
+
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-g" 'magit-status)
+
+(require 'windmove)
+(windmove-default-keybindings)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(provide 'my-keybindings)
